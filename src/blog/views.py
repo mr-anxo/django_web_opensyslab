f532f1e7b6cf4eeefc8023f8981e2ace0ee0c140
Axel Ahin <mail.ahinaxel@gmail.com>
2024-06-21 00:42:33 +0000

    --- Auto Git Commit ---

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from blog.models import Articles
+from blog.models import Articles, Category
 
 # Create your views here.
 
 def blog_home(request):
     articles = Articles.objects.filter(published=True)
-    return render(request, "blog-home.html", context={ "articles" : articles })
+    categories = Category.objects.all()
+    return render(request, "blog-home.html", context={ "articles" : articles, "categories" : categories })
 
 
 def blog_article(request, article_slug):
